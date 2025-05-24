1698f5feab885ffa3f75ff0852d0522ff143ca6f
UnEpicNotDeveloper <joshuah.dev@gmail.com>
2025-05-24 00:15:10 -0400

    working image

--- app/page.tsx
+++ app/page.tsx
@@ -2,63 +2,47 @@
 import { pageState } from "../store/pageState"
 import Link from "next/link"
 import React from 'react';
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
-import ImageHotspots from "react-image-hotspots";
-import Navbar from "../page-components/Navbar"
 import Image from "next/image"
 
+
+// @ts-ignore
+const pibImg = require('../public/pib.png');
 
 export default function Page() {
   return (
     <div style={{ padding: 20 }}>
       <h2>All possible ways to display pib.png</h2>
-      {/* 1. Using next/image with relative path */}
+      {/* Only using next/image with require syntax */}
+      <div>
+        <h3>next/image (require syntax)</h3>
+        <Image src={pibImg} alt="pib" width={300} height={200} />
+      </div>
+      {/* 
+      // The following usages are now commented out to only use next/image with require syntax
       <div>
         <h3>next/image (relative path)</h3>
         <Image src="/pib.png" alt="pib" width={300} height={200} />
       </div>
-      {/* 2. Using next/image with require (if pib.png is in /public) */}
-      <div>
-        <h3>next/image (require syntax)</h3>
-        {/* @ts-ignore */}
-        <Image src={require('../public/pib.png')} alt="pib" width={300} height={200} />
-      </div>
-      {/* 3. Using HTML img tag with relative path */}
       <div>
         <h3>img tag (relative path)</h3>
         <img src="/pib.png" alt="pib" width={300} height={200} />
       </div>
-      {/* 4. Using HTML img tag with import (if pib.png is imported as a module) */}
       <div>
         <h3>img tag (imported as module)</h3>
-        {/* 
-        // @ts-ignore
-        import pibImg from '../../public/pib.png';
-        <img src={pibImg} alt="pib" width={300} height={200} />
-        */}
         <div style={{ color: "#888" }}>Not supported in Next.js app directory by default</div>
       </div>
-      {/* 5. Using next/image with import (if using webpack file-loader) */}
       <div>
         <h3>next/image (imported as module)</h3>
-        {/* 
-        // @ts-ignore
-        import pibImg from '../../public/pib.png';
-        <Image src={pibImg} alt="pib" width={300} height={200} />
-        */}
         <div style={{ color: "#888" }}>Not supported in Next.js app directory by default</div>
       </div>
-      {/* 6. Using next/image with absolute URL */}
       <div>
         <h3>next/image (absolute URL)</h3>
         <Image src="https://unepicnotdeveloper.github.io/biology-web/pib.png" alt="pib" width={300} height={200} />
       </div>
-      {/* 7. Using img tag with absolute URL */}
       <div>
         <h3>img tag (absolute URL)</h3>
         <img src="https://unepicnotdeveloper.github.io/biology-web/pib.png" alt="pib" width={300} height={200} />
       </div>
-      {/* 8. Using background-image in a div */}
       <div>
         <h3>div with background-image (relative path)</h3>
         <div
@@ -71,7 +55,6 @@ export default function Page() {
           }}
         />
       </div>
-      {/* 9. Using background-image in a div (absolute URL) */}
       <div>
         <h3>div with background-image (absolute URL)</h3>
         <div
@@ -84,6 +67,7 @@ export default function Page() {
           }}
         />
       </div>
+      */}
     </div>
   );
 }
