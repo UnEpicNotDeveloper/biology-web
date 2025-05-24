8e185687e6667dabc6a329ab9868486b98fc2b28
UnEpicNotDeveloper <joshuah.dev@gmail.com>
2025-05-23 21:28:45 -0400

    next images

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from 'next';
 
+const withImage=require('next-images')
+module.exports = withImage()
+
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
